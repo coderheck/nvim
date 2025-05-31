a05224d52fb1312c858ca6ccc04134f8981d380f
coderheck <loxzyioxzybusiness@hotmail.com>
2025-05-31 23:30:48 +0700

    first commit

---
+++ init.lua
@@ -0,0 +1,70 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "number"
+vim.opt.expandtab = false
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.colorcolumn = "70"
+vim.g.mapleader = ","
+vim.api.nvim_set_option("clipboard", "unnamedplus")
+vim.keymap.set('n', "<C-H>", "db", { noremap = true })
+vim.keymap.set('n', "<C-Del>", "dw", { noremap = true })
+vim.keymap.set('n', '<C-a>', 'ggVG', { noremap = true })
+vim.keymap.set('n', '.e', ':Neotree toggle<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>qq', ':wqa<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>q!', ':qa!<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<M-Up>', ':m .-2<CR>==', { noremap = true, silent = true })
+vim.keymap.set('n', '<M-Down>', ':m .+1<CR>==', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>r', function()
+    vim.cmd("w")
+    local runFile = vim.fn.expand("%:t:r")
+    vim.cmd("term gpp " .. runFile)
+end, { noremap = true })
+vim.keymap.set('i', '<C-H>', '<C-w>', { noremap = true })
+vim.keymap.set('i', "<C-Del>", "<C-o>dw", { noremap = true })
+vim.keymap.set('i', '<C-a>', '<Esc>ggVG', { noremap = true })
+vim.keymap.set('v', '<M-Up>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set('v', '<M-Down>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set("v", "<Tab>", ">gv", { noremap = true, silent = true })
+vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
+require("config.lazy")
+require 'nt-cpp-tools'.setup({
+    preview = {
+        quit = 'q', 
+        accept = '<tab>'
+    },
+    header_extension = 'h',
+    source_extension = 'cxx',
+    custom_define_class_function_commands = {
+        TSCppImplWrite = {
+            output_handle = require'nt-cpp-tools.output_handlers'.get_add_to_cpp()
+        }
+        --[[
+        <your impl {
+            output_handle = function (str, context) 
+                -- string contains the class implementation
+                -- do whatever you want to do with it
+            end
+        }
+        ]]
+    }
+})
+-- cmdline completion
+local cmp = require("cmp")
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'path' },
+    { name = 'cmdline' },
+  },
+})
+local lspconfig = require("lspconfig")
+lspconfig.clangd.setup({
+  capabilities = require("cmp_nvim_lsp").default_capabilities(),
+})
+vim.diagnostic.config({ signs = false })
+vim.o.background = "dark"
+vim.cmd([[colorscheme vscode]])
+-- vim.cmd([[Neotree show]])
---
+++ lua/config/lazy.lua
@@ -0,0 +1,124 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = ","
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+		{ "tpope/vim-sleuth" },
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = 
+      {
+	"nvim-lua/plenary.nvim",
+	"nvim-tree/nvim-web-devicons",
+	"MunifTanjim/nui.nvim",
+      },
+      lazy = false,
+      ---@module "neo-tree"
+      ---@type neotree.Config?
+      opts = {},
+    },
+    {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+      config = function()
+	local configs = require("nvim-treesitter.configs")
+	configs.setup({
+	  ensure_installed = "all",
+	  highlight = { enable = true },
+	  indent = { enable = true },
+	  autotag = { 
+	    enable = true,
+	    enable_close_on_slash = false 
+	  },
+	})
+      end,
+    },
+    {
+      "Badhi/nvim-treesitter-cpp-tools",
+      dependencies = { "nvim-treesitter/nvim-treesitter" },
+      opts = function()
+					local options = {
+          preview = {
+            quit = "q",
+            accept = "<tab>",
+          },
+          header_extension = "h",
+          source_extension = "cpp",
+          custom_define_class_function_commands = {
+            TSCppImplWrite = {
+              output_handle = require("nt-cpp-tools.output_handlers").get_add_to_cpp(),
+            },
+          },
+        }
+        return options
+      end,
+      config = true,
+    },
+    { "Mofiqul/vscode.nvim", priority = 1000, config = true, opts = ... },
+    {
+      "hrsh7th/nvim-cmp",
+      dependencies = {
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-cmdline",
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip"
+      },
+      config = function()
+	local cmp = require("cmp")
+	cmp.setup({
+	  snippet = {
+	    expand = function(args)
+	      require("luasnip").lsp_expand(args.body)
+	    end
+	  },
+	mapping = cmp.mapping.preset.insert({
+	  ['<C-Space>'] = cmp.mapping.complete(),
+	  ['<CR>'] = cmp.mapping.confirm({ select = true }),
+	  ['<Tab>'] = cmp.mapping.select_next_item(),
+	  ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+	}),
+	sources = cmp.config.sources({
+	  { name = 'nvim_lsp' },
+	  { name = 'luasnip' },
+	}, {
+	  { name = 'buffer' },
+	  { name = 'path' },
+	}),
+      })
+      end,
+    },
+    {
+      "neovim/nvim-lspconfig",
+      dependencies = {"williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim",}
+    },
+  },
+  install = { colorscheme = { "vscode" } },
+  checker = { enabled = true },
+})
